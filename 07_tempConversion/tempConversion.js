0ae5b47de82b3b458e6dc098dcb9991db615c8b4
kubais <30256909+kubais@users.noreply.github.com>
2024-02-29 16:41:33 +0100

    Add formula to convert from F to C and C to F.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fart, cels) {
+   cels = ((fart - 32) * 5/9);
+   let num = Math.round(cels * 10) / 10;
+   return num;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fart, cels) {
+   fart = (cels * 9/5 + 35);
+   let num = Math.round(fart * 10) / 10;
+  return num
 };
 
 // Do not edit below this line

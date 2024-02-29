2a03f11800f49dd404c275e88207a4e427e3796d
kubais <30256909+kubais@users.noreply.github.com>
2024-02-29 10:00:10 +0100

    Make function that sums all numbers within two values.
    Use loop to itinirate between two values.
    Use condition if(!Number.isInteger()) to get error when numbers
    is not and integer.
    To flip arguments' position use array deconstruction [x, y] = [y, x]
    or const temp = x; x = y; y = temp.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
 
-};
+
+const sumAll = function(x, y) {
+
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+    if (x < 0 || y < 0) return "ERROR";
+    if (x > y) {
+        [x, y] = [y, x];
+        // const temp = x;
+        // x = y;
+        // y = temp;
+    }
+
+    // second way to flip values: if (x > y) [x, y] = [y, x];
+let sum = 0;
+for (let i = x; i <= y; i++)
+sum += (i);
+return sum;
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;

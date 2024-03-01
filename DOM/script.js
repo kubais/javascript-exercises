880057f79bf78db955016b508fbadab7a22bae6b
kubais <30256909+kubais@users.noreply.github.com>
2024-03-01 16:31:26 +0100

    Select all buttons, iterate through each and add click listener.

--- DOM/script.js
+++ DOM/script.js
@@ -66,3 +66,15 @@ function alertFunction() {
   btn3.addEventListener('click', function (e) {
     e.target.style.background = 'blue';
   });
+
+  // buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});

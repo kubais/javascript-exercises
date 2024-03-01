c9066c123dda5c01d706a2cc2eaa428ad5cdbd9a
kubais <30256909+kubais@users.noreply.github.com>
2024-03-01 08:48:15 +0100

    Tackle tasks and pass all the tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return y += x;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return y = x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(x,n) {
+	return x = (x ** n);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+  
 };
 
 // Do not edit below this line

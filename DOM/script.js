68ed3de64a8061f9369ee1931d78f1a54b2ad7dc
kubais <30256909+kubais@users.noreply.github.com>
2024-03-01 15:56:32 +0100

    Add different nodes using DOM manipulations.

--- DOM/script.js
+++ DOM/script.js
@@ -3,12 +3,27 @@ const container = document.querySelector('#container');
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
-
 container.appendChild(content);
-
 
 const para = document.createElement('p');
 para.style.color = 'red';
 para.textContent = `Hey I'm red!`;
-
 container.appendChild(para);
+
+const h3 = document.createElement('h3');
+h3.style.color = 'blue';
+h3.textContent = `I'm a blue h3`;
+container.appendChild(h3);
+
+let div2 = document.createElement('div');
+div2.textContent = 'div2';
+container.appendChild(div2);
+
+let h1 = document.querySelector('div2');
+h1 = document.createElement('h1');
+h1.textContent = `I'm a div`;
+
+const para2 = document.createElement('p');
+para2.textContent = 'ME TOO';
+
+div2.append(h1, para2);

fdd9fac3b8e79be70094066c5c8316ae87abdf3e
kubais <30256909+kubais@users.noreply.github.com>
2024-03-01 11:31:10 +0100

    Add para with red text.

--- DOM/script.js
+++ DOM/script.js
@@ -5,3 +5,10 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+
+const para = document.createElement('p');
+para.style.color = 'red';
+para.textContent = `Hey I'm red!`;
+
+container.appendChild(para);
